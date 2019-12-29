9f8ca50bfe3f8eae61f78381828bf22e654c4ba9
Sruthi Jaganathan <sjaganat@ucsc.edu>
2019-12-28 18:05:56 -0800

    Mobile calendar app start by Sruthi & John

---
+++ AppCalendar/AppCalendar/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCalendar
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+            PrintConsole.Clicked += delegate
+            {
+
+                var eventName = name.Text;
+                Console.WriteLine("eventName: " + eventName);
+
+                var startTime = start.Text;
+                Console.WriteLine("startTime: " + startTime);
+
+                var endTime = end.Text;
+                Console.WriteLine("endTime: " + endTime);
+
+
+            };
+   
+        }
+    }
+}
