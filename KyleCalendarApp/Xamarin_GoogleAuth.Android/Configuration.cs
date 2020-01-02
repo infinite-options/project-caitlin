a5ef5a7733adc6cee42d085a22758bb827d5ddaf
Kyle Hoefer <31253287+kylehoefer@users.noreply.github.com>
2020-01-02 13:04:10 -0800

    Removed KEY
    Please message me to receive the API connection key for this file

--- KyleCalendarApp/Xamarin_GoogleAuth.Android/Configuration.cs
+++ KyleCalendarApp/Xamarin_GoogleAuth.Android/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class Configuration
     {
-        public const string ClientId = "REQUEST KEY FROM ME";
+        public const string ClientId = "REQUEST KEY FROM ME PLEASE";
         public const string Scope = "https://www.googleapis.com/auth/calendar";
         public const string RedirectUrl = "com.woodenmoose.xamarin.googleauth:/oauth2redirect";
         //public const string RedirectUrl = "urn:ietf:wg:oauth:2.0:oob";
