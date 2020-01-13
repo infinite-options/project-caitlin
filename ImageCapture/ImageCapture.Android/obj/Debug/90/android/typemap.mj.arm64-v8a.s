c9b1129ad9ac05b9819a2d0525a9edec2b9c950b
Sruthi Jaganathan <sjaganat@ucsc.edu>
2020-01-13 14:29:15 -0800

    google photos api section of code

---
+++ ImageCapture/ImageCapture.Android/obj/Debug/90/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 6e671320cffe4a19a85c0a4259d6a9b5b07ca974 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1422
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372565
+	.include	"typemap.mj.inc"
---
+++ ImageCapture/ImageCapture/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using static Android.Support.V4.Media.MediaBrowserCompat;
+
+namespace ImageCapture
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Take_Pic(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsTakePhotoSupported && !CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Message", "Photo capture and Pick not supported", "ok");
+                return;
+            } else
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Directory = "Images",
+                    Name = DateTime.Now + "_pic.jpg"
+
+                });
+
+                if (file == null)
+                    return;
+
+                await DisplayAlert("File Path", file.Path, "Ok");
+                MyImage.Source = ImageSource.FromStream(() =>
+
+                {
+
+                    var stream = file.GetStream();
+                    return stream;
+
+                });
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("No Upload", "Picking photo is not supported", "Ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+            MyImage.Source = ImageSource.FromStream(() => file.GetStream());
+
+        }
+    }
+}
